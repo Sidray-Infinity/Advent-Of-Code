14cbbf3e44a48b0b1e1051eedc2805819ba0e6fd
Sidray-Infinity <sidkiit12@gmail.com>
2020-12-26 18:10:25 +0530

    day 15 part1

--- 2020/15.py
+++ 2020/15.py
@@ -1,38 +1,50 @@
 
+def getNthNumberSpoken(n, data):
+    turnCount = 1
+    ageTracker = {}
+    for d in data:
+        if ageTracker.get(d, None) != None:
+            if ageTracker[d][1] is None:
+                ageTracker[d][1] = turnCount
+            else:
+                ageTracker[d][0] = ageTracker[d][1]
+                ageTracker[d][1] = turnCount
+        else:
+            ageTracker[d] = [turnCount, None]
+        turnCount += 1
+    
+    lastNum = data[-1]
+
+    while turnCount <= n:
+        print(turnCount)
+        if ageTracker[lastNum][1] == None:
+            lastNum = 0
+            if ageTracker.get(0, None) != None:
+                if ageTracker[0][1] is None:
+                    ageTracker[0][1] = turnCount
+                else:
+                    ageTracker[0][0] = ageTracker[0][1]
+                    ageTracker[0][1] = turnCount
+            else:
+                ageTracker[0] = [turnCount, None]
+        else:
+            lastNum = ageTracker[lastNum][1] - ageTracker[lastNum][0]
+            if ageTracker.get(lastNum, None) != None:
+                if ageTracker[lastNum][1] is None:
+                    ageTracker[lastNum][1] = turnCount
+                else:
+                    ageTracker[lastNum][0] = ageTracker[lastNum][1]
+                    ageTracker[lastNum][1] = turnCount
+            else:
+                ageTracker[lastNum] = [turnCount, None]
+
+
+        turnCount += 1
+
+    return lastNum
 
 if __name__ == "__main__":
     data = list(map(int, open("15.txt").read().split(',')))
-    ageTracker = {}
-
-    for d in data:
-        for k in ageTracker.keys():
-            ageTracker[k][0] += 1
-        ageTracker[d] = [1, False]
-        
-
-    print(ageTracker)
-
-    numCount = 3
-    lastNum = data[-1]
-
-    while numCount != 2020:
-
-        if ageTracker.get(lastNum, None) != None:
-            if ageTracker[lastNum][1] == False:
-                ageTracker[0][1] = True
-                ageTracker[0][0] = 0
-                lastNum = 0
-            else:
-                t = lastNum
-                lastNum = numCount - 1 - ageTracker[lastNum][0]
-                ageTracker[t][0] = 0
-        else:
-            ageTracker[lastNum] = [0, False]
-
-        for k in ageTracker.keys():
-            ageTracker[k][0] += 1
-        print(numCount, lastNum)
-        numCount += 1
-        
-
-    print("Part1:", lastNum)
+    
+    print('Part1:', getNthNumberSpoken(2020, data))
+    print('Part2:', getNthNumberSpoken(30000000, data))
